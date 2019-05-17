1c57ab931b5aaa0bf8a731c15d40da35fc659765
jcrcs <juancarlosrosito@gmail.com>
2019-05-17 11:54:07 +0200

    add jupyter dir

--- python-workspace/main.py
+++ python-workspace/main.py
@@ -2,6 +2,10 @@ from get_dbpedia import *
 from TypesClass import *
 
 
+# For Jupyter
+# import os
+# os.chdir('c:\\ws\\schema-annotation-workspace\\schema_annotation\\python-workspace')
+
 
 def main():
     table = Table()
